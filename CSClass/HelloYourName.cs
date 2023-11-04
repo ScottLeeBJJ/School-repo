cfb17d67935a87ed39880a8132349e64152a05d2
Scott Lee <scottlee5455@outlook.com>
2023-11-04 17:43:36 -0500

    Attempting to use dotnet build

--- CSClass/HelloYourName.cs
+++ CSClass/HelloYourName.cs
@@ -1,3 +1,16 @@
+<Window x:Class="HelloYourName.MainWindow"
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        Title="Hello Your Name" Height="200" Width="350">
+    <Grid>
+        <Label Content="Enter your name:" HorizontalAlignment="Left" VerticalAlignment="Top" Margin="20,20,0,0"/>
+        <TextBox x:Name="nameTextBox" HorizontalAlignment="Left" VerticalAlignment="Top" Width="200" Margin="20,50,0,0"/>
+        <Button Content="Say Hello" HorizontalAlignment="Left" VerticalAlignment="Top" Width="100" Height="30" Margin="20,90,0,0" Click="SayHello_Click"/>
+    </Grid>
+</Window>
+
+
+
 using System.Windows;
 
 namespace HelloYourName
