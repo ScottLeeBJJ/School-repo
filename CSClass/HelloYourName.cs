0e642952da20dbb14824169d7a7707bcb289e124
Scott Lee <scottlee5455@outlook.com>
2023-11-04 18:22:07 -0500

    trouble getting C# to build in vs code

--- CSClass/HelloYourName.cs
+++ CSClass/HelloYourName.cs
@@ -1,17 +1,5 @@
-<Window x:Class="HelloYourName.MainWindow"
-        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
-        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
-        Title="Hello Your Name" Height="200" Width="350">
-    <Grid>
-        <Label Content="Enter your name:" HorizontalAlignment="Left" VerticalAlignment="Top" Margin="20,20,0,0"/>
-        <TextBox x:Name="nameTextBox" HorizontalAlignment="Left" VerticalAlignment="Top" Width="200" Margin="20,50,0,0"/>
-        <Button Content="Say Hello" HorizontalAlignment="Left" VerticalAlignment="Top" Width="100" Height="30" Margin="20,90,0,0" Click="SayHello_Click"/>
-    </Grid>
-</Window>
-
-
-
 using System.Windows;
+using System.Windows.Controls;
 
 namespace HelloYourName
 {
@@ -22,17 +10,25 @@ namespace HelloYourName
             InitializeComponent();
         }
 
-        private void SayHello_Click(object sender, RoutedEventArgs e)
+        private async void SayHello_Click(object sender, RoutedEventArgs e)
         {
             string userName = nameTextBox.Text;
 
             if (!string.IsNullOrEmpty(userName))
             {
-                MessageBox.Show("Hello, " + userName + "!", "Greetings");
+                ContentDialog dialog = new ContentDialog
+                {
+                    Title = "Greetings",
+                    Content = $"Hello, {userName}!",
+                    PrimaryButtonText = "OK"
+                };
+
+                await dialog.ShowAsync();
             }
             else
             {
-                MessageBox.Show("Please enter your name.", "Error");
+                nameTextBox.BorderBrush = new SolidColorBrush(Colors.Red);
+                nameErrorTextBlock.Visibility = Visibility.Visible;
             }
         }
     }
