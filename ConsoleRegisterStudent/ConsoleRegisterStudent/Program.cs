c8a4ee3e3a38392138a85abaa6dfeddfbae7e309
Scott Lee <56131751+ScottLeeBJJ@users.noreply.github.com>
2023-11-24 10:48:51 -0600

    Working on updating the project

--- ConsoleRegisterStudent/ConsoleRegisterStudent/Program.cs
+++ ConsoleRegisterStudent/ConsoleRegisterStudent/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleRegisterStudent
 {
@@ -10,55 +6,84 @@ namespace ConsoleRegisterStudent
     {
         static void Main(string[] args)
         {
+            // Entry point of the program
             (new Program()).run();
         }
 
-
         void run()
         {
-            int choice;
+            // Variables to track user choices and credit hours
+            int choice = 0;
             int firstChoice = 0, secondChoice = 0, thirdChoice = 0;
             int totalCredit = 0;
             string yesOrNo = "";
 
+            // Initial message
             System.Console.WriteLine("Teacher's Copy");
 
             do
             {
+                // Display course menu and prompt for user input
                 WritePrompt();
-                choice = Convert.ToInt32(Console.ReadLine());
 
-                switch (ValidateChoice(choice, firstChoice, secondChoice, thirdChoice, totalCredit))
+                // Read user input and validate it
+                if (int.TryParse(Console.ReadLine(), out choice))
                 {
-                    case -1:
-                        Console.WriteLine("Your entered selection {0} is not a recognized course.", choice);
-                        break;
-                    case -2:
-                        Console.WriteLine("You have already registerd for this {0} course.", ChoiceToCourse(choice));
-                        break;
-                    case -3:
-                        Console.WriteLine("You can not register for more than 9 credit hours.");
-                        break;
-                    case 0:
-                        Console.WriteLine("Registration Confirmed for course {0}.", ChoiceToCourse(choice));
-                        totalCredit += 3;
-                        if (firstChoice == 0)
-                            firstChoice = choice;
-                        else if (secondChoice == 0)
-                            secondChoice = choice;
-                        else if (thirdChoice == 0)
-                            thirdChoice = choice;
-                        break;
+                    // Validate the user choice and perform actions accordingly
+                    int validationResult = ValidateChoice(choice, firstChoice, secondChoice, thirdChoice, totalCredit);
+
+                    switch (validationResult)
+                    {
+                        case 0:
+                            // If choice is valid, register the student for the course
+                            Console.WriteLine("Registration Confirmed for course {0}.", ChoiceToCourse(choice));
+                            totalCredit += 3;
+
+                            // Update the first, second, or third choice based on availability
+                            if (firstChoice == 0)
+                                firstChoice = choice;
+                            else if (secondChoice == 0)
+                                secondChoice = choice;
+                            else if (thirdChoice == 0)
+                                thirdChoice = choice;
+                            break;
+                        case -1:
+                            // If choice is not recognized, display an error message
+                            Console.WriteLine("Your entered selection {0} is not a recognized course.", choice);
+                            break;
+                        case -2:
+                            // If already registered for the course, display an error message
+                            Console.WriteLine("You have already registered for this {0} course.", ChoiceToCourse(choice));
+                            break;
+                        case -3:
+                            // If exceeding credit limit, display an error message
+                            Console.WriteLine("You cannot register for more than 9 credit hours.");
+                            break;
+                        default:
+                            // Handle unexpected validation results
+                            Console.WriteLine("Unexpected validation result: {0}", validationResult);
+                            break;
+                    }
+
+                    // Display the current registration status
+                    WriteCurrentRegistration(firstChoice, secondChoice, thirdChoice);
+                }
+                else
+                {
+                    // Handle invalid input (non-integer)
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
                 }
 
-                WriteCurrentRegistration(firstChoice, secondChoice, thirdChoice);
+                // Prompt the user to register for another course
                 Console.Write("\nDo you want to try again? (Y|N)? : ");
                 yesOrNo = (Console.ReadLine()).ToUpper();
             } while (yesOrNo == "Y");
 
+            // Display a thank you message when the user finishes registering
             Console.WriteLine("Thank you for registering with us");
         }
 
+        // Display the course menu
         void WritePrompt()
         {
             Console.WriteLine("Please select a course for which you want to register by typing the number inside []");
@@ -66,18 +91,19 @@ namespace ConsoleRegisterStudent
             Console.Write("Enter your choice : ");
         }
 
+        // Validate the user choice based on business rules
         int ValidateChoice(int choice, int firstChoice, int secondChoice, int thirdChoice, int totalCredit)
         {
-            if (choice < 1 || choice > 70)
-                return -1;
-            else if (choice == firstChoice && choice == secondChoice && choice == thirdChoice)
-                return -2;
-            else if (totalCredit > 9)
-                return -3;
-            return -4;
+            if (choice < 1 || choice > 7)
+                return -1; // Choice not recognized
+            else if (choice == firstChoice || choice == secondChoice || choice == thirdChoice)
+                return -2; // Already registered for the course
+            else if (totalCredit + 3 > 9)
+                return -3; // Exceeding credit limit
+            return 0; // Valid choice
         }
 
-
+        // Display the current registration status
         void WriteCurrentRegistration(int firstChoice, int secondChoice, int thirdChoice)
         {
             if (secondChoice == 0)
@@ -88,6 +114,7 @@ namespace ConsoleRegisterStudent
                 Console.WriteLine("You are currently registered for {0}, {1}, {2}", ChoiceToCourse(firstChoice), ChoiceToCourse(secondChoice), ChoiceToCourse(thirdChoice));
         }
 
+        // Convert course choice to course name
         string ChoiceToCourse(int choice)
         {
             string course = "";
