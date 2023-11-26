1edb4b4f17244c9cb56edb35dbe4aaf65b8d4dab
Scott Lee <56131751+ScottLeeBJJ@users.noreply.github.com>
2023-11-26 15:35:11 -0600

    working on the project

--- ConsoleRegisterStudent/ConsoleRegisterStudent/ConsoleRegisterStudent/Program.cs
+++ ConsoleRegisterStudent/ConsoleRegisterStudent/ConsoleRegisterStudent/Program.cs
@@ -4,30 +4,39 @@ namespace ConsoleRegisterStudent
 {
     class Program
     {
+        // Constants
+        const int CreditHoursPerCourse = 3;
+        const int CreditHoursLimit = 9;
+
+        // Array to map choices to course names
+        static string[] CourseNames = { "", "IT 145", "IT 200", "IT 201", "IT 270", "IT 315", "IT 328", "IT 330" };
+
+        // Entry point of the program
         static void Main(string[] args)
         {
-            // Entry point of the program
-            (new Program()).run();
+            // Create an instance of the Program class and start the program
+            (new Program()).Run();
         }
 
-        void run()
+        // Main program logic
+        void Run()
         {
             // Variables to track user choices and credit hours
-            int choice = 0;
             int firstChoice = 0, secondChoice = 0, thirdChoice = 0;
             int totalCredit = 0;
             string yesOrNo = "";
 
             // Initial message
-            System.Console.WriteLine("Teacher's Copy");
+            Console.WriteLine("Teacher's Copy");
 
+            // Main loop for course registration
             do
             {
                 // Display course menu and prompt for user input
                 WritePrompt();
 
                 // Read user input and validate it
-                if (int.TryParse(Console.ReadLine(), out choice))
+                if (int.TryParse(Console.ReadLine(), out int choice))
                 {
                     // Validate the user choice and perform actions accordingly
                     int validationResult = ValidateChoice(choice, firstChoice, secondChoice, thirdChoice, totalCredit);
@@ -36,8 +45,8 @@ namespace ConsoleRegisterStudent
                     {
                         case 0:
                             // If choice is valid, register the student for the course
-                            Console.WriteLine("Registration Confirmed for course {0}.", ChoiceToCourse(choice));
-                            totalCredit += 3;
+                            Console.WriteLine($"Registration Confirmed for course {ChoiceToCourse(choice)}.");
+                            totalCredit += CreditHoursPerCourse;
 
                             // Update the first, second, or third choice based on availability
                             if (firstChoice == 0)
@@ -49,11 +58,11 @@ namespace ConsoleRegisterStudent
                             break;
                         case -1:
                             // If choice is not recognized, display an error message
-                            Console.WriteLine("Your entered selection {0} is not a recognized course.", choice);
+                            Console.WriteLine($"Your entered selection {choice} is not a recognized course.");
                             break;
                         case -2:
                             // If already registered for the course, display an error message
-                            Console.WriteLine("You have already registered for this {0} course.", ChoiceToCourse(choice));
+                            Console.WriteLine($"You have already registered for {ChoiceToCourse(choice)} course.");
                             break;
                         case -3:
                             // If exceeding credit limit, display an error message
@@ -61,7 +70,7 @@ namespace ConsoleRegisterStudent
                             break;
                         default:
                             // Handle unexpected validation results
-                            Console.WriteLine("Unexpected validation result: {0}", validationResult);
+                            Console.WriteLine($"Unexpected validation result: {validationResult}");
                             break;
                     }
 
@@ -76,7 +85,7 @@ namespace ConsoleRegisterStudent
 
                 // Prompt the user to register for another course
                 Console.Write("\nDo you want to try again? (Y|N)? : ");
-                yesOrNo = (Console.ReadLine()).ToUpper();
+                yesOrNo = Console.ReadLine().ToUpper();
             } while (yesOrNo == "Y");
 
             // Display a thank you message when the user finishes registering
@@ -87,7 +96,7 @@ namespace ConsoleRegisterStudent
         void WritePrompt()
         {
             Console.WriteLine("Please select a course for which you want to register by typing the number inside []");
-            Console.WriteLine("[1]IT 145\n[2]IT 200\n[3]IT 201\n[4]IT 270\n[5]IT 315\n[6]IT 328\n[7]IT 330");
+            Console.WriteLine("[1] IT 145\n[2] IT 200\n[3] IT 201\n[4] IT 270\n[5] IT 315\n[6] IT 328\n[7] IT 330");
             Console.Write("Enter your choice : ");
         }
 
@@ -98,7 +107,7 @@ namespace ConsoleRegisterStudent
                 return -1; // Choice not recognized
             else if (choice == firstChoice || choice == secondChoice || choice == thirdChoice)
                 return -2; // Already registered for the course
-            else if (totalCredit + 3 > 9)
+            else if (totalCredit + CreditHoursPerCourse > CreditHoursLimit)
                 return -3; // Exceeding credit limit
             return 0; // Valid choice
         }
@@ -106,45 +115,19 @@ namespace ConsoleRegisterStudent
         // Display the current registration status
         void WriteCurrentRegistration(int firstChoice, int secondChoice, int thirdChoice)
         {
+            Console.Write("You are currently registered for ");
             if (secondChoice == 0)
-                Console.WriteLine("You are currently registered for {0}", ChoiceToCourse(firstChoice));
+                Console.WriteLine($"{ChoiceToCourse(firstChoice)}");
             else if (thirdChoice == 0)
-                Console.WriteLine("You are currently registered for {0}, {1}", ChoiceToCourse(firstChoice), ChoiceToCourse(secondChoice));
+                Console.WriteLine($"{ChoiceToCourse(firstChoice)}, {ChoiceToCourse(secondChoice)}");
             else
-                Console.WriteLine("You are currently registered for {0}, {1}, {2}", ChoiceToCourse(firstChoice), ChoiceToCourse(secondChoice), ChoiceToCourse(thirdChoice));
+                Console.WriteLine($"{ChoiceToCourse(firstChoice)}, {ChoiceToCourse(secondChoice)}, {ChoiceToCourse(thirdChoice)}");
         }
 
         // Convert course choice to course name
         string ChoiceToCourse(int choice)
         {
-            string course = "";
-            switch (choice)
-            {
-                case 1:
-                    course = "IT 145";
-                    break;
-                case 2:
-                    course = "IT 200";
-                    break;
-                case 3:
-                    course = "IT 201";
-                    break;
-                case 4:
-                    course = "IT 270";
-                    break;
-                case 5:
-                    course = "IT 315";
-                    break;
-                case 6:
-                    course = "IT 328";
-                    break;
-                case 7:
-                    course = "IT 330";
-                    break;
-                default:
-                    break;
-            }
-            return course;
+            return CourseNames[choice];
         }
     }
 }
