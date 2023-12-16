39e0f6d28f7f3b2de211434370d22898919dc020
Scott Lee <scottlee5455@outlook.com>
2023-12-16 12:06:27 -0600

    working on final

--- wpfregisterstudent/WPFRegisterStudent/WPFRegisterStudent/App.xaml.cs
+++ wpfregisterstudent/WPFRegisterStudent/WPFRegisterStudent/App.xaml.cs
@@ -1,17 +1,88 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace WPFRegisterStudent
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        [STAThread]
+        public static void Main()
+        {
+            App app = new App();
+            MainWindow mainWindow = new MainWindow();
+            app.Run(mainWindow);
+        }
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Grade { get; set; }
+    }
+
+    public partial class MainWindow : Window
+    {
+        private TextBox nameTextBox;
+        private TextBox ageTextBox;
+        private TextBox gradeTextBox;
+
+        public MainWindow()
+        {
+            InitializeUI();
+        }
+
+        private void InitializeUI()
+        {
+            // Initialize UI elements here (e.g., textboxes, labels, buttons)
+
+            // Example:
+            nameTextBox = new TextBox();
+            ageTextBox = new TextBox();
+            gradeTextBox = new TextBox();
+
+            Button registerButton = new Button();
+            registerButton.Content = "Register";
+            registerButton.Click += RegisterButton_Click;
+
+            // Add UI elements to the window (e.g., using a Grid)
+            // ...
+
+            Content = new Grid(); // Replace with the appropriate layout
+
+            // Example:
+            ((Grid)Content).Children.Add(nameTextBox);
+            ((Grid)Content).Children.Add(ageTextBox);
+            ((Grid)Content).Children.Add(gradeTextBox);
+            ((Grid)Content).Children.Add(registerButton);
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Validate input and create a Student object
+            if (int.TryParse(ageTextBox.Text, out int age) && IsValidGrade(gradeTextBox.Text))
+            {
+                Student student = new Student
+                {
+                    Name = nameTextBox.Text,
+                    Age = age,
+                    Grade = gradeTextBox.Text
+                };
+
+                // Display student information in a MessageBox
+                MessageBox.Show($"Student Details:\nName: {student.Name}\nAge: {student.Age}\nGrade: {student.Grade}", "Registration Successful");
+            }
+            else
+            {
+                MessageBox.Show("Invalid input. Please enter valid information.", "Error");
+            }
+        }
+
+        private bool IsValidGrade(string grade)
+        {
+            // Implement grade validation logic (e.g., check against a list of valid grades)
+            // Return true if valid, false otherwise
+            return !string.IsNullOrWhiteSpace(grade); // Placeholder logic, replace with actual validation
+        }
     }
 }
--- wpfregisterstudent/WPFRegisterStudent/WPFRegisterStudent/Course.cs
+++ wpfregisterstudent/WPFRegisterStudent/WPFRegisterStudent/Course.cs
@@ -1,44 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WPFRegisterStudent
 {
     class Course
     {
-        private string name = "";
-        private bool isRegisteredAlready = false;
+        private string name;
+        private bool isRegisteredAlready;
 
+        // Use properties instead of explicit getter and setter methods
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public bool IsRegisteredAlready
+        {
+            get { return isRegisteredAlready; }
+        }
+
+        // Use a constructor to set initial values
         public Course(string name)
         {
             this.name = name;
+            this.isRegisteredAlready = false; // Initialize to false by default
         }
 
-        public void setName(string name)
-        {
-            this.name = name;
-        }
+        // You can remove the setName method since the property setter is available
 
-        public string getName()
-        {
-            return name;
-        }
-
-        public bool IsRegisteredAlready()
-        {
-            return isRegisteredAlready;
-        }
+        // You can remove the getName method since the property getter is available
 
         public void SetToRegistered()
         {
             isRegisteredAlready = true;
         }
 
+        // Override the ToString method for better string representation
         public override string ToString()
         {
-            return getName();
+            return Name;
         }
     }
 }
