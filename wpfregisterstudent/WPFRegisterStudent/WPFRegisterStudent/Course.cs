d9d107a10825873cca1eb7466a5903860ed797d1
Scott Lee <scottlee5455@outlook.com>
2023-12-16 12:08:58 -0600

    finished

--- wpfregisterstudent/WPFRegisterStudent/WPFRegisterStudent/Course.cs
+++ wpfregisterstudent/WPFRegisterStudent/WPFRegisterStudent/Course.cs
@@ -7,7 +7,6 @@ namespace WPFRegisterStudent
         private string name;
         private bool isRegisteredAlready;
 
-        // Use properties instead of explicit getter and setter methods
         public string Name
         {
             get { return name; }
@@ -19,23 +18,17 @@ namespace WPFRegisterStudent
             get { return isRegisteredAlready; }
         }
 
-        // Use a constructor to set initial values
         public Course(string name)
         {
             this.name = name;
-            this.isRegisteredAlready = false; // Initialize to false by default
+            this.isRegisteredAlready = false;
         }
-
-        // You can remove the setName method since the property setter is available
-
-        // You can remove the getName method since the property getter is available
 
         public void SetToRegistered()
         {
             isRegisteredAlready = true;
         }
 
-        // Override the ToString method for better string representation
         public override string ToString()
         {
             return Name;
--- wpfregisterstudent/WPFRegisterStudent/WPFRegisterStudent/MainWindow.xaml.cs
+++ wpfregisterstudent/WPFRegisterStudent/WPFRegisterStudent/MainWindow.xaml.cs
@@ -1,27 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPFRegisterStudent
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        Course choice;
-
+        private Course choice;
 
         public MainWindow()
         {
@@ -30,7 +14,6 @@ namespace WPFRegisterStudent
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-
             Course course1 = new Course("IT 145");
             Course course2 = new Course("IT 200");
             Course course3 = new Course("IT 201");
@@ -38,7 +21,6 @@ namespace WPFRegisterStudent
             Course course5 = new Course("IT 315");
             Course course6 = new Course("IT 328");
             Course course7 = new Course("IT 330");
-
 
             this.comboBox.Items.Add(course1);
             this.comboBox.Items.Add(course2);
@@ -48,19 +30,30 @@ namespace WPFRegisterStudent
             this.comboBox.Items.Add(course6);
             this.comboBox.Items.Add(course7);
 
-
             this.textBox.Text = "";
+            this.totalCreditHoursTextBox.Text = "0";
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
             choice = (Course)(this.comboBox.SelectedItem);
 
-            // TO DO - Create code to validate user selection (the choice object)
-            // and to display an error or a registation confirmation message accordinlgy
-            // Also update the total credit hours textbox if registration is confirmed for a selected course
+            if (choice != null && !choice.IsRegisteredAlready)
+            {
+                choice.SetToRegistered();
+                MessageBox.Show($"Registration confirmed for {choice.Name}", "Success");
 
+                int currentCreditHours = int.Parse(this.totalCreditHoursTextBox.Text);
+                this.totalCreditHoursTextBox.Text = (currentCreditHours + 3).ToString();
+            }
+            else if (choice != null && choice.IsRegisteredAlready)
+            {
+                MessageBox.Show($"{choice.Name} is already registered", "Error");
+            }
+            else
+            {
+                MessageBox.Show("Please select a course", "Error");
+            }
         }
-
     }
 }
